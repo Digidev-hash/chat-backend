8dca42ab1462e34cac1f5fc040da53769a0f27ea
Digidev-hash <digicitydev@gmail.com>
2025-02-07 00:35:18 +0100

    web socket fix

--- chat/routing.py
+++ chat/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<conversation_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<conversation_id>\w+)/$', consumers.ChatConsumer.as_asgi()),
   
 ]
 
